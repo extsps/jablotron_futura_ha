6d11c3718446eb809c6474347cbdaee966e468cc
Evgeniy Labunskiy <extsps@gmail.com>
2024-11-27 15:31:17 +0200

    Update __init__.py

--- custom_components/jablotron_futura/__init__.py
+++ custom_components/jablotron_futura/__init__.py
@@ -1,9 +1,12 @@
+import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import ConfigType
-import logging
+from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT
+from .modbus import ModbusHub
 
 DOMAIN = "jablotron_futura"
+DEFAULT_PORT = 502
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
@@ -13,11 +16,14 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Jablotron Futura from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    return await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    modbus_hub = ModbusHub(hass, entry.data[CONF_IP_ADDRESS], entry.data[CONF_PORT])
+    hass.data[DOMAIN][entry.entry_id] = modbus_hub
+    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload Jablotron Futura config entry."""
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
