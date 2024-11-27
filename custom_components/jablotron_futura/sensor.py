b3148862e28294b7cc70f9e44ef8aa2623fe47b3
Evgeniy Labunskiy <extsps@gmail.com>
2024-11-27 15:21:34 +0200

    Update sensor.py

--- custom_components/jablotron_futura/sensor.py
+++ custom_components/jablotron_futura/sensor.py
@@ -1,32 +1,15 @@
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import TEMP_CELSIUS
+from . import DOMAIN
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    hub = hass.data["jablotron_futura"]
-    async_add_entities([
-        JablotronFuturaSensor(hub, "Temperature", "temp_indoor", TEMP_CELSIUS)
-    ])
-
-class JablotronFuturaSensor(SensorEntity):
-    def __init__(self, hub, name, register, unit):
-        self._hub = hub
-        self._name = name
-        self._register = register
-        self._unit = unit
-        self._state = None
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        return self._unit
-
-    async def async_update(self):
-        result = await self._hub.read_register(self._register)
-        self._state = result
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the Jablotron Futura sensors from config entry."""
+    modbus_hub = hass.data[DOMAIN]
+    async_add_entities(
+        [
+            JablotronFuturaSensor(modbus_hub, "Ambient Temperature", 30, TEMP_CELSIUS),
+            JablotronFuturaSensor(modbus_hub, "Fresh Air Temperature", 31, TEMP_CELSIUS),
+            # Add more sensors as required
+        ],
+        update_before_add=True,
+    )
